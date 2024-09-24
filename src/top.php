2152b81ee8810d60d9bc4f91caa49c4c8cbe67c1
Anderson Nunes <dande.nunes.94@gmail.com>
2024-09-24 16:41:58 -0300

    Adição e implementação do componente css -  normalize.css

--- src/top.php
+++ src/top.php
@@ -9,6 +9,7 @@
     <meta property="og:description" content="Desenvolvimento de soluções onlines pra negócios"/>
     <meta property="og:url" content="http://lipsam.com.br"/>
     <meta property="og:image" content="http://lipsam.com.br/img/og-image.png"/>
+    <link rel="stylesheet" href="../assets/css/normalize.css/normalize.css">
     <title>Lipsam - Desenvolvimento de soluções onlines pra negócios</title>
 </head>
 <body>
